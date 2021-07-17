8625d11f946f73a997820cdb90096e6f935eca73
ssongd-creater <sksk1440@gmail.com>
2021-07-17 23:17:45 +0900

    7/17 button jquery 구현은 했으나 오류 해결중

--- jq_main.js
+++ jq_main.js
@@ -14,18 +14,28 @@ $(function () {
           "right": "0px"
         }
       );
-      $(".main_button").html("<i class="+"fa"+"fa - angle - down >"+"</i > ")
+      $(".main_button").removeClass('btnup');
+      $(".main_button").addClass('btndown');
+      $(".main_button").html('<i class= "fa fa-angle-up"></i>');
     } else {
+      //$(".main_button").addClass = ('btneff');
+
       $(".main_button").css(
         {
           "position": "absolute",
           "background": "#8d2125",
           "left": "50%",
           "transform": "translateX(-50%)",
-          "right": "auto"
+          "right": "auto",
+          "color": "#fff",
+          "font-size": "14px",
+          "font-weight": "500"
         }
       );
-      $(".main_button").html("SCROLL DOWN < i class="+"fa fa-angle-down"+ "></i >");
+      $(".main_button").removeClass('btndown');
+      $(".main_button").addClass('btnup');
+      $(".main_button").html('SCROLL DOWN <i class= "fa fa-angle-down"></i>');
+      
     }
   });
 
