e0e9255ad389383d5861c3dec89d0d86feb2888c
ssongd-creater <sksk1440@gmail.com>
2021-08-04 18:36:16 +0900

    8/3 반응형 작업중 !

--- jq_main.js
+++ jq_main.js
@@ -1,8 +1,16 @@
 $(function () {
   
+  $("#main_button").click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
+
   $(window).on("scroll", function () {
     let scroll_btn = $(this).scrollTop();
 
+    //Button Scroll Event Jquery
     //console.log(scroll_btn);
     if (scroll_btn !== 0) {
       $(".main_button").removeClass('btnup');
@@ -12,20 +20,20 @@ $(function () {
       $(".info_img").addClass('show_ani');
     } else {
       //$(".main_button").addClass = ('btneff');
-
       $(".main_button").removeClass('btndown');
       $(".main_button").addClass('btnup');
       $(".main_button").html('SCROLL DOWN <i class= "fa fa-angle-down"></i>');
     }
 
-    // History Scroll event
+    
+    
 
+    // History Scroll event
     if (scroll_btn >= 1090) {
       $(".sub_infoimg").show();
       $(".sub_infoimg").addClass('show_ani');
       $(".sub_infoimg .sub_imgs").addClass('show_ani');
       $(".sub_infoimg .sub_imgs div").addClass('show_ani');
-
     };
 
     if (scroll_btn >= 2030) {
@@ -39,7 +47,7 @@ $(function () {
       $(".right_history_1st").addClass("active_right_1");
     } else {
       $(".right_history_1st").removeClass("active_right_1");
-    }
+    };
 
     if (scroll_btn >= 2340) {
       $(".left_history_1st").removeClass("active_left_1");
@@ -47,7 +55,7 @@ $(function () {
       $(".left_history_2nd").addClass("active_left_1");
     } else {
       $(".left_history_2nd").removeClass("active_left_1");
-    }
+    };
 
     if (scroll_btn >= 2540) {
       $(".left_history_1st").removeClass("active_left_1");
@@ -56,7 +64,7 @@ $(function () {
       $(".right_history_2nd").addClass("active_right_1");
     } else {
       $(".right_history_2nd").removeClass("active_right_1");
-    }
+    };
 
     if (scroll_btn >= 2830) {
       $(".left_history_1st").removeClass("active_left_1");
@@ -66,7 +74,7 @@ $(function () {
       $(".middle_logo").addClass("active_middle");
     } else {
       $(".middle_logo").removeClass("active_middle");
-    }
+    };
 
     if (scroll_btn >= 3250) {
       $(".left_history_1st").removeClass("active_left_1");
@@ -77,7 +85,7 @@ $(function () {
       $(".left_history_3rd").addClass("active_left_1");
     } else {
       $(".left_history_3rd").removeClass("active_left_1");
-    }
+    };
 
     if (scroll_btn >= 3430) {
       $(".left_history_1st").removeClass("active_left_1");
@@ -89,7 +97,7 @@ $(function () {
       $(".right_history_3rd").addClass("active_right_1");
     } else {
       $(".right_history_3rd").removeClass("active_right_1");
-    }
+    };
 
     if (scroll_btn >= 3650) {
       $(".left_history_1st").removeClass("active_left_1");
@@ -102,7 +110,7 @@ $(function () {
       $(".left_history_4rd").addClass("active_left_1");
     } else {
       $(".left_history_4rd").removeClass("active_left_1");
-    }
+    };
 
     if (scroll_btn >= 3830) {
       $(".left_history_1st").removeClass("active_left_1");
@@ -116,9 +124,10 @@ $(function () {
       $(".right_history_4rd").addClass("active_right_1");
     } else {
       $(".right_history_4rd").removeClass("active_right_1");
-    }
+    };
   });
 
+  //Main Page Slide Section
 
 
   
